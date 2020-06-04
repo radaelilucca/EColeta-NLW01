e26f49f872518b50c03f51e650c55ff44367de71
Lucca Radaeli <radaelidev@gmail.com>
2020-06-03 21:40:13 -0300

    Added static image for points

--- backend/src/app/controllers/PointController.ts
+++ backend/src/app/controllers/PointController.ts
@@ -18,7 +18,7 @@ class Point {
     } = req.body;
 
     const point = {
-      image: 'image-fake',  
+      image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',  
       name,
       email,
       whatsapp,
